e2a31e763bd8a4a1a712ddc931c2d430418b9919
jlegacy <jlegacy@suddenlink.net>
2016-11-24 18:39:22 -0500

    First semi working project
    First semi working project

--- angular2-seed/app/challenge.js
+++ angular2-seed/app/challenge.js
@@ -1,6 +1,8 @@
 var keyPad = [];
+var keyPadWorking = [];
 var rows = [];
 var sequences = [];
+var workingSequences = [];
 var rowCount = 4;
 var rulesArray = [];
 var invalidSequences = [];
@@ -117,7 +119,65 @@ keyPad[3].push('M');
 keyPad[3].push('N');
 keyPad[3].push('');
 
-var verifyValidCells = function(dir, col, row, spaces) {
+var generateSequence = function (start, n) {
+  var printSequence = [];
+  var endCharacter = "";
+  var searchCharacter = "";
+
+  workingSequences = sequences.slice(0);
+
+  printSequence.push(keyPad[start.row][start.column]);
+
+  //set initial end//
+
+  for (x in workingSequences) {
+    if (workingSequences[x]["start"] === keyPad[start.row][start.column]) {
+      printSequence.push(workingSequences[x]["end"]);
+      workingSequences.splice(x,1);
+      break;
+    }
+  }
+
+  //create sequence//
+  for (var z = 1; z < n; z++) {
+
+    for (x in workingSequences) {
+
+      if (workingSequences[x]["start"] === searchCharacter) {
+        printSequence.push(workingSequences[x]["end"]);
+        workingSequences.splice(x,1);
+      }
+
+      searchCharacter = printSequence[printSequence.length - 1];
+
+    }
+
+  }
+
+  console.log(printSequence);
+
+}
+
+
+
+
+var printSequence = function (keyValue) {
+  var foundObj = {};
+  for (var row = 0; row < keyPad.length; row++) {
+    for (var column = 0; column < keyPad[row].length; column++) {
+      if (keyPad[row][column] === keyValue) {
+        foundObj.row = row;
+        foundObj.column = column;
+        return foundObj;
+
+      }
+    }
+  }
+  return null;
+
+}
+
+var verifyValidCells = function (dir, col, row, spaces) {
   var invalid = false;
   switch (dir) {
     case 'north':
@@ -139,7 +199,7 @@ var verifyValidCells = function(dir, col, row, spaces) {
       if (!invalid)
         return row - index;
       return null;
-      
+
     case 'south':
       var z = 0;
       var index = 0;
@@ -201,7 +261,7 @@ var verifyValidCells = function(dir, col, row, spaces) {
   }
 }
 
-var createRuleEndPoints = function(row, column) {
+var createRuleEndPoints = function (row, column) {
   console.log('creatingEndPoints');
   var colStartRow, colStartCol;
   if (keyPad[row][column] > '') {
@@ -235,16 +295,19 @@ var createRuleEndPoints = function(row, column) {
           colStartCol = verifyValidCells(rulesArray[x].dir2.position, column, colStartRow, rulesArray[x].dir2.spaces);
           break;
         case 'west':
-         colStartCol = verifyValidCells(rulesArray[x].dir2.position, column, colStartRow, rulesArray[x].dir2.spaces);
+          colStartCol = verifyValidCells(rulesArray[x].dir2.position, column, colStartRow, rulesArray[x].dir2.spaces);
           break;
 
-      } 
+      }
 
       if (typeof keyPad[colStartRow] !== 'undefined') {
         if (typeof keyPad[colStartRow][colStartCol] !== 'undefined') {
 
           if (keyPad[colStartRow][colStartCol] > '') {
-            console.log('found', keyPad[colStartRow][colStartCol]);
+            // console.log('found it start', keyPad[row][column]);
+            console.log('found it end', keyPad[colStartRow][colStartCol]);
+            sequences.push({ 'start': keyPad[row][column], 'end': keyPad[colStartRow][colStartCol] })
+
           }
 
         }
@@ -255,11 +318,20 @@ var createRuleEndPoints = function(row, column) {
 
 for (x in keyPad) {
   for (var z = 0; z < keyPad[x].length; z++) {
-    if (parseInt(x) === 2 && z === 3) {
-      createRuleEndPoints(parseInt(x), z);
-    }
+    //if (parseInt(x) === 2 && z === 3) {
+    createRuleEndPoints(parseInt(x), z);
+    // }
+
+
+
 
   }
 
+  var foundStart = printSequence('A');
+  console.log(foundStart);
+
+  generateSequence(foundStart, 10);
+
+  console.log(sequences);
 
 }
